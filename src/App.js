71b1aaee6df96281620587d4b74c65e40d588e77
Yastremsky Vitaly <yastremsky@gambit.ru>
2017-08-19 21:39:11 +0300

    base fetch api, selected data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import io from 'socket.io-client';
 import * as firebase from 'firebase'
 import ws from './utils/ws'
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 
+var options = [
+  { value: 'one', label: 'One' },
+  { value: 'two', label: 'Two' }
+];
 
+function logChange(val) {
+  console.log("Selected: " + JSON.stringify(val));
+}
   // var config = {
   //   apiKey: "AIzaSyD-HSSoQ99IhZW4VYh1YFDxBCszQJIuIB4",
   //   authDomain: "bip-trade.firebaseapp.com",
@@ -16,15 +25,20 @@ import ws from './utils/ws'
   //   messagingSenderId: "24158785314"
   // };
   // firebase.initializeApp(config);
-
+const List = () => {
+  
+}
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      speed: 10
+      speed: 10,
+      fxRates: []
     }
   }
-
+  componentWillMount() {
+    io("http://localhost:3005").on("data", data => this.setState({ fxRates: data }));
+  }
   // componentDidMount() {
   //   console.log(firebase.database().ref().child('bip-trade'))
   //   const rootRef = firebase.database().ref();
@@ -38,19 +52,39 @@ class App extends Component {
   //   })
   // }
 
+
   render() {
+    // console.log(this.state.fxRates)
+    let data = [];
+    let filterPare = [];
+    if(this.state.fxRates !== null) {
+       data = this.state.fxRates.result
+       filterPare = typeof data !== "undefined" ? data.map(item => ({value: item.MarketName, label: item.MarketName})) : []
+    }
+
+    
+    // console.log(filterPare)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          <span>    
-            {this.state.speed}
-          </span>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Select
+          name="form-field-name"
+          value="one"
+          options={filterPare}
+          onChange={logChange}
+        />
+        {/* <ul>
+           { typeof data === "undefined" ? <li> No data</li> :
+            data.map((item, i) => {
+              return (
+                <li key={i}>
+                  <a className="marketName">
+                    {item.MarketName} 
+                  </a> 
+                </li>
+              )
+            })
+           }
+        </ul>   */}
       </div>
     );
   }
---
+++ src/server.js
@@ -0,0 +1,51 @@
+var express = require('express'),
+app = express(),
+http = require('http').Server(app),
+io = require('socket.io')(http),
+fetch = require('node-fetch');
+
+var bittrex = require('./node.bittrex.api/node.bittrex.api.js');
+
+
+
+// DEFAULT SOCKET
+var cor = __dirname.slice(0, __dirname.length - 3) + 'public';
+app.use(express.static(__dirname + '/'));
+
+var fields = ["currencyPair", "timestamp", "bidBig", "bidPips", "offerBig", "offerPips", "high", "low", "open"]
+var cachedData;
+var connected = 0;
+
+var interval = setInterval(updateData, 10000);
+
+io.on('connection', function (socket) {
+connected++;
+io.sockets.emit('data', cachedData);
+socket.on('disconnect', function () {
+  connected--;
+});
+});
+
+const processData = (fields, valid) => data => {
+  // console.log(data)
+  return data;
+  // data.split("\n").map(row => row.split(",")
+  // .reduce((acc, val, i) => { acc[fields[i]] = val; return acc }, {}))
+  // .filter(obj => obj.hasOwnProperty(valid))
+}
+
+
+function updateData() {
+fetch('https://bittrex.com/api/v1.1/public/getmarketsummaries')
+  .then(response=> response.json())
+  // .then(processData(fields, "timestamp"))
+  .then(result => {
+    cachedData = result;
+    // console.log(result)
+    io.sockets.emit('data', result);
+})
+}
+
+http.listen(3005, function () {
+console.log('listening on: 3000');
+});
